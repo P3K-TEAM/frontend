3d98fa9d0367cc0c79520469a77db4c12efd3a9d
František Gič <mcfreddie777@gmail.com>
2020-11-03 16:52:07 +0100

    FRON-16 Add prettier and eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,37 @@ module.exports = {
 	env: {
 		node: true
 	},
+	extends: [
+		'plugin:vue/essential',
+		'plugin:vue/recommended',
+		'eslint:recommended'
+	],
+	plugins: ['vue'],
 	parserOptions: {
 		parser: 'babel-eslint'
 	},
 	rules: {
+		'vue/max-attributes-per-line': [
+			2,
+			{
+				singleline: 2,
+				multiline: {
+					max: 1,
+					allowFirstLine: false
+				}
+			}
+		],
+		'vue/html-indent': [
+			'error',
+			'tab',
+			{
+				attribute: 1,
+				baseIndent: 1,
+				closeBracket: 0,
+				alignAttributesVertically: true,
+				ignores: []
+			}
+		],
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
 	}
