b8dfea8113ad29d3ce380fe70e65ca33ac938385
H3MII <rusnak.anton3@gmail.com>
2020-11-05 16:35:06 +0100

    FRON-15 refactor code

--- src/components/Result/ResultDocument.vue
+++ src/components/Result/ResultDocument.vue
@@ -1,27 +1,27 @@
 <template>
 	<div class="mx-auto w-2/4 py-24">
 		<div class="pb-4 pl-2 hover:text-accent">
-			<a href="/results" class="text-center text-gray-700 ">
-
+			<a href="/results" class="text-center text-gray-700">
 				<i class="fas fa-angle-left text-2xl" />
 				<span class="text-bold text-2xl">Všetky súbory</span>
 			</a>
 		</div>
 		<div class="shadow-lg">
-			<div class="flex bg-blue-own p-2 ">
-				<span class="w-1/4 pl-2 text-white ">
-					{{ name }}
-				</span>
-				<div class="w-1/4 ml-auto text-right">
-					<span class=" text-white"> Zhoda:</span>
-					<span class=" text-white font-bold text-xl">
-						{{ percentage }}
-					</span>
-					<span class="pr-2 text-white font-bold text-xl">%
+			<div class="flex bg-blue-own p-2">
+				<div class="w-1/4 ">
+					<span class="pl-2 text-white">
+						{{ name }}
 					</span>
 				</div>
+				<div class="w-1/4 ml-auto text-right">
+					<span class="text-white"> Zhoda:</span>
+					<span class="text-white font-bold text-xl">
+						{{ percentage }}
+					</span>
+					<span class="pr-2 text-white font-bold text-xl">% </span>
+				</div>
 			</div>
-			<div class="text-sm p-8 bg-white text-justify ">
+			<div class="text-sm p-8 bg-white text-justify">
 				{{ text }}
 			</div>
 		</div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,18 @@
 			<div class="flex mx-auto">
 				<div class="w-1/2 mr-auto text-white ">
 					<p class="text-3xl pb-4">Výsledky kontroly</p>
-					<p class="text-md ">
+					<p class="text-md">
 						Gratulujeme. Nižšie nájdete podrobné štatistiky kontroly
 						originality vášej práce.
 					</p>
 				</div>
-				<div class="w-1/2 ">
-					<div
-						class="flex bg-blue-own px-4 rounded-lg w-1/2 ml-auto text-center "
-					>
-						<span class=" w-full inline-block text-sm text-white align-middle">Vaša zhoda je </span>
-						<span class="w-full inline-block font-bold text-4xl text-white">{{ zhoda }}</span>
-						<span class="inline-block font-bold text-4xl text-white">%</span>
+				<div
+					class="w-1/4 bg-blue-own px-4 rounded-lg ml-auto my-4"
+				>
+					<div class=" text-center mx-auto pt-2">
+						<p class="text-sm text-white">Vaša zhoda je </p>
+						<span class="font-bold text-4xl text-white">{{ zhoda }}</span>
+						<span class="font-bold text-4xl text-white">%</span>
 					</div>
 				</div>
 			</div>
@@ -29,56 +29,54 @@
 </template>
 
 <script>
-import ResultWhole from '../components/Result/ResultWhole';
-import ResultDocument from '../components/Result/ResultDocument';
-import ResultDocItem from '../components/Result/ResultDocItem';
-export default {
-	components: {
-		ResultWhole,
-		ResultDocument,
-		ResultDocItem,
-	},
-	data: function () {
-		return {
-			zhoda: 0.55,
-			document: {
-				name: 'Document1.pdf',
-				percentage: 5,
-				text:
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla semper odio quis pulvinar mattis. Sed vulputate arcu eget porttitor fringilla. ' +
-					'Donec efficitur, odio ut cursus mattis, magna lorem convallis purus, nec varius turpis urna sed velit. Quisque convallis dolor et ex accumsan volutpat.' +
-					' Suspendisse ullamcorper nibh quis sagittis maximus. Maecenas a vehicula felis. Aliquam vel eros molestie, dignissim est et, suscipit leo.' +
-					' Aenean quis leo ut metus sagittis convallis in vel elit. Aenean et metus dolor. Vestibulum et congue quam. In ornare pellentesque odio. ' +
-					'Integer non urna ultrices, sollicitudin dui vitae, gravida orci. Nullam tellus eros, iaculis vitae velit eu, tincidunt ultrices sapien. ' +
-					'Donec non mauris ac massa pulvinar rutrum. Sed maximus velit ut lacus porta blandit. Etiam dolor orci, fermentum a porttitor sed, elementum quis sem.\n' +
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla semper odio quis pulvinar mattis. Sed vulputate arcu eget porttitor fringilla.',
-			},
-		};
-	},
-};
+	import ResultDocument from '../components/Result/ResultDocument';
+
+	export default {
+		components: {
+			ResultDocument,
+		},
+		data: function() {
+			return {
+				zhoda: 0.55,
+				document: {
+					name: 'Document1.pdf',
+					percentage: 5,
+					text:
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla semper odio quis pulvinar mattis. Sed vulputate arcu eget porttitor fringilla. ' +
+						'Donec efficitur, odio ut cursus mattis, magna lorem convallis purus, nec varius turpis urna sed velit. Quisque convallis dolor et ex accumsan volutpat.' +
+						' Suspendisse ullamcorper nibh quis sagittis maximus. Maecenas a vehicula felis. Aliquam vel eros molestie, dignissim est et, suscipit leo.' +
+						' Aenean quis leo ut metus sagittis convallis in vel elit. Aenean et metus dolor. Vestibulum et congue quam. In ornare pellentesque odio. ' +
+						'Integer non urna ultrices, sollicitudin dui vitae, gravida orci. Nullam tellus eros, iaculis vitae velit eu, tincidunt ultrices sapien. ' +
+						'Donec non mauris ac massa pulvinar rutrum. Sed maximus velit ut lacus porta blandit. Etiam dolor orci, fermentum a porttitor sed, elementum quis sem.\n' +
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla semper odio quis pulvinar mattis. Sed vulputate arcu eget porttitor fringilla.'
+				}
+			};
+		}
+	};
 </script>
 
 <style scoped>
-.skewed-top-wrapper {
-	clip-path: polygon(
-		0 0,
-		100% 0,
-		100% 90%,
-		85% 95%,
-		68% 98%,
-		50% 100%,
-		32% 98%,
-		15% 95%,
-		0 90%
-	);
-	background: rgb(0, 25, 64);
-	background: linear-gradient(
-		90deg,
-		rgba(0, 25, 64, 1) 0%,
-		rgba(0, 49, 128, 1) 93%
-	);
-}
-.bg-blue-own {
-	background: #1699f1;
-}
+	.skewed-top-wrapper {
+		clip-path: polygon(
+			0 0,
+			100% 0,
+			100% 90%,
+			85% 95%,
+			68% 98%,
+			50% 100%,
+			32% 98%,
+			15% 95%,
+			0 90%
+		);
+		background: rgb(0, 25, 64);
+		background: linear-gradient(
+			90deg,
+			rgba(0, 25, 64, 1) 0%,
+			rgba(0, 49, 128, 1) 93%
+		);
+	}
+
+	.bg-blue-own {
+		background: #1699f1;
+	}
 </style>
