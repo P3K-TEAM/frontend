949319c55f1d9577121833426cd9d79bf570f95b
František Gič <mcfreddie777@gmail.com>
2020-11-08 00:43:39 +0100

    Set proper color scheme into project
    Signed-off-by: František Gič <mcfreddie777@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,17 @@ module.exports = {
 				3: '3px',
 			},
 			colors: {
-				primary: '#1699F1',
-				'primary-dark': '#003180',
-				footer: '#03132C',
+				white: '#fdfdfd',
+				primary: {
+					100: '#d5f4ff',
+					200: '#8ed4fc',
+					300: '#61c8fd',
+					400: '#36c3ff',
+					500: '#00b5ff',
+					600: '#1699F1',
+					700: '#003180',
+					900: '#03132C',
+				},
 			},
 			width: {
 				96: '24rem',
@@ -22,6 +30,12 @@ module.exports = {
 				128: '32rem',
 				144: '36rem',
 				160: '40rem',
+			},
+			fontSize: {
+				'7xl': '5rem',
+				'8xl': '6rem',
+				'9xl': '7rem',
+				'10xl': '8rem',
 			},
 		},
 	},
