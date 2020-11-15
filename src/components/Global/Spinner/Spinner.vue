388c9fa7c7757073fc412302e32b04df095c27c1
František Gič <mcfreddie777@gmail.com>
2020-11-15 15:33:08 +0100

    FRON-20 Implement global loading spinner with global backdrop

---
+++ src/components/Global/Spinner/Spinner.vue
@@ -0,0 +1,20 @@
+<template>
+	<svg
+		v-if="isLoading"
+		viewBox="0 0 100 100"
+		xmlns="http://www.w3.org/2000/svg"
+		class="text-primary-500 stroke-current spinner w-16"
+	>
+		<circle cx="50" cy="50" r="45" />
+	</svg>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+	computed: {
+		...mapGetters(['isLoading']),
+	},
+};
+</script>
