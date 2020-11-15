ce7d2cd7f4e4d3cde446b2af405970e1a47a48f3
František Gič <mcfreddie777@gmail.com>
2020-11-15 13:43:18 +0100

    FRON-20 Implement modal component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,29 @@
 	<div id="app" class="flex flex-col h-full">
 		<router-view class="flex-auto" />
 		<Alert />
+		<Modal />
 	</div>
 </template>
 
 <script>
 import Alert from '@/components/Alert/Alert';
+import Modal from '@/components/Modal/Modal';
 
 export default {
 	components: {
 		Alert,
+		Modal,
+	},
+	mounted() {
+		this.$store.dispatch('ModalStore/setModal', {
+			header: '',
+			text:
+				'Lord, yer not fighting me without a beauty! Small, mysterious dosis',
+			confirmation: {
+				enabled: true,
+				callback: () => {},
+			},
+		});
 	},
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import Vuex from 'vuex';
 
 // stores
 import AlertStore from './alert/alert.store';
+import ModalStore from './modal/modal.store';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
 	modules: {
 		AlertStore,
+		ModalStore,
 	},
 });
