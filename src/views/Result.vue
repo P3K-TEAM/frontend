f4c19de397672f20de321f13f2121e3624d1d1de
H3MII <rusnak.anton3@gmail.com>
2020-11-16 22:11:04 +0100

    §FRON-18 refactor code

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="bg-gray-300 min-h-screen">
 		<div class="skewed-top-wrapper bg-primary-gradient text-white">
-			<Navigation />
+			<Navigation/>
 			<div
 				class="container flex justify-between items-center mx-auto pt-16 pb-32"
 			>
@@ -20,11 +20,11 @@
 					<div
 						class="flex justify-between py-1 text-gray-600 text-md font-bold"
 					>
-						<span class="w-1/2 ml-8"> Nazov </span>
+						<span class="w-1/2 ml-8 pl-6"> Nazov </span>
 						<span class="w-1/4 text-center">
 							Percentuálna zhoda
 						</span>
-						<span class="w-1/4 text-center"> Počet zhôd </span>
+						<span class="w-1/4 text-center mr-4"> Počet zhôd </span>
 					</div>
 					<div v-for="doc in documents" :key="doc.id">
 						<router-link
@@ -32,8 +32,10 @@
 							class="flex justify-between mb-3 p-2 text-black bg-white shadow rounded-lg hover-trigger"
 							append
 						>
-							<i :class="filetype_i(doc.name)"></i>
-							<span class="w-1/2 ml-5">
+							<span class="w-8 text-center mx-2">
+								<i :class="filetype_i(doc.name)" ></i>
+							</span>
+							<span class="w-1/2">
 								{{ doc.name }}
 							</span>
 							<span class="w-1/4 text-center">
@@ -42,8 +44,8 @@
 							<span class="w-1/4 text-center">
 								{{ doc.matched_docs.length }}
 							</span>
-							<div class="w-2">
-								<i class="fas fa-angle-right hover-target"></i>
+							<div class="w-2 mr-2">
+								<i class="fas fa-angle-right hover-target text-2xl text-gray-600"></i>
 							</div>
 						</router-link>
 					</div>
@@ -63,136 +65,136 @@
 </template>
 
 <script>
-import Navigation from '@/components/Navigation/Navigation';
+	import Navigation from '@/components/Navigation/Navigation';
 
-export default {
-	components: {
-		Navigation,
-	},
-	data: function () {
-		return {
-			id_result: 11,
-			documents: [
-				{
-					id: 1,
-					name: 'Document2.pdf',
-					percentage: 0.15,
-					text:
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dapibus consequat ullamcorper. Proin a erat nunc. Aenean at gravida lorem,' +
-						' vel iaculis lorem. Quisque bibendum suscipit velit in tincidunt. Quisque ut ipsum egestas risus pretium dignissim. Aliquam sit amet nibh eget felis ' +
-						'dignissim ultrices. Cras ac ultricies libero. Sed eu tincidunt leo. Vivamus vestibulum dictum nisl ac tempus. Vestibulum ante ipsum primis' +
-						' in faucibus orci luctus et ultrices posuere cubilia curae; Cras posuere consectetur nibh, vel molestie lacus finibus et. Nullam lectus mi,' +
-						' aliquam quis enim ac, elementum vehicula metus.',
-					matched_docs: [
-						{
-							name: '1.pdf',
-							percentage: 0.45,
-
-							matches: [
-								{
-									char_from: 15,
-									char_to: 19,
-								},
-								{
-									char_from: 15,
-									char_to: 19,
-								},
-							],
-						},
-						{
-							name: '2.pdf',
-							percentage: 0.45,
-							matches: [
-								{
-									char_from: 15,
-									char_to: 19,
-								},
-								{
-									char_from: 15,
-									char_to: 19,
-								},
-							],
-						},
-					],
-				},
-				{
-					id: 2,
-					name: 'Document2.docx',
-					percentage: 0.15,
-					text:
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dapibus consequat ullamcorper. Proin a erat nunc. Aenean at gravida lorem,' +
-						' vel iaculis lorem. Quisque bibendum suscipit velit in tincidunt. Quisque ut ipsum egestas risus pretium dignissim. Aliquam sit amet nibh eget felis ' +
-						'dignissim ultrices. Cras ac ultricies libero. Sed eu tincidunt leo. Vivamus vestibulum dictum nisl ac tempus. Vestibulum ante ipsum primis' +
-						' in faucibus orci luctus et ultrices posuere cubilia curae; Cras posuere consectetur nibh, vel molestie lacus finibus et. Nullam lectus mi,' +
-						' aliquam quis enim ac, elementum vehicula metus.',
-					matched_docs: [
-						{
-							name: '1.pdf',
-							percentage: 0.45,
-
-							matches: [
-								{
-									char_from: 15,
-									char_to: 19,
-								},
-								{
-									char_from: 15,
-									char_to: 19,
-								},
-							],
-						},
-						{
-							name: '2.pdf',
-							percentage: 0.45,
-							matches: [
-								{
-									char_from: 15,
-									char_to: 19,
-								},
-								{
-									char_from: 15,
-									char_to: 19,
-								},
-							],
-						},
-					],
-				},
-			],
-		};
-	},
-	methods: {
-		filetype_i: function (text) {
-
-
-			var regex = /\.(.[^.]*)$/;
-			var match = text.match(regex);
-			console.log(match[0])
-			console.log(Object(text))
-
-			switch (match[0]) {
-				case '.pdf':
-					return 'fas fa-file-pdf';
-				case '.docx':
-				case '.doc':
-					return 'fas fa-file';
-				default:
-					return 'fas fa-file';
-			}
+	export default {
+		components: {
+			Navigation
 		},
-	},
-};
+		data: function() {
+			return {
+				id_result: 11,
+				documents: [
+					{
+						id: 1,
+						name: 'Docum.ent2.pdf',
+						percentage: 0.15,
+						text:
+							'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dapibus consequat ullamcorper. Proin a erat nunc. Aenean at gravida lorem,' +
+							' vel iaculis lorem. Quisque bibendum suscipit velit in tincidunt. Quisque ut ipsum egestas risus pretium dignissim. Aliquam sit amet nibh eget felis ' +
+							'dignissim ultrices. Cras ac ultricies libero. Sed eu tincidunt leo. Vivamus vestibulum dictum nisl ac tempus. Vestibulum ante ipsum primis' +
+							' in faucibus orci luctus et ultrices posuere cubilia curae; Cras posuere consectetur nibh, vel molestie lacus finibus et. Nullam lectus mi,' +
+							' aliquam quis enim ac, elementum vehicula metus.',
+						matched_docs: [
+							{
+								name: '1.pdf',
+								percentage: 0.45,
+
+								matches: [
+									{
+										char_from: 15,
+										char_to: 19
+									},
+									{
+										char_from: 15,
+										char_to: 19
+									}
+								]
+							},
+							{
+								name: '2.pdf',
+								percentage: 0.45,
+								matches: [
+									{
+										char_from: 15,
+										char_to: 19
+									},
+									{
+										char_from: 15,
+										char_to: 19
+									}
+								]
+							}
+						]
+					},
+					{
+						id: 2,
+						name: 'Docu.ment2.docx',
+						percentage: 0.15,
+						text:
+							'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dapibus consequat ullamcorper. Proin a erat nunc. Aenean at gravida lorem,' +
+							' vel iaculis lorem. Quisque bibendum suscipit velit in tincidunt. Quisque ut ipsum egestas risus pretium dignissim. Aliquam sit amet nibh eget felis ' +
+							'dignissim ultrices. Cras ac ultricies libero. Sed eu tincidunt leo. Vivamus vestibulum dictum nisl ac tempus. Vestibulum ante ipsum primis' +
+							' in faucibus orci luctus et ultrices posuere cubilia curae; Cras posuere consectetur nibh, vel molestie lacus finibus et. Nullam lectus mi,' +
+							' aliquam quis enim ac, elementum vehicula metus.',
+						matched_docs: [
+							{
+								name: '1.pdf',
+								percentage: 0.45,
+
+								matches: [
+									{
+										char_from: 15,
+										char_to: 19
+									},
+									{
+										char_from: 15,
+										char_to: 19
+									}
+								]
+							},
+							{
+								name: '2.pdf',
+								percentage: 0.45,
+								matches: [
+									{
+										char_from: 15,
+										char_to: 19
+									},
+									{
+										char_from: 15,
+										char_to: 19
+									}
+								]
+							}
+						]
+					}
+				]
+			};
+		},
+		methods: {
+			filetype_i: function(text) {
+
+
+				var regex = /\.(.[^.]*)$/;
+				var match = text.match(regex);
+				console.log(match[0]);
+				console.log(Object(text));
+
+				switch (match[0]) {
+					case '.pdf':
+						return 'fas fa-file-pdf text-red-500 text-2xl';
+					case '.docx':
+					case '.doc':
+						return 'fas fa-file text-blue-500 text-2xl';
+					default:
+						return 'fas fa-file text-2x ';
+				}
+			}
+		}
+	};
 </script>
 
 <style scoped>
-.skewed-top-wrapper {
-	clip-path: ellipse(95% 100% at 50% 0%);
-}
+	.skewed-top-wrapper {
+		clip-path: ellipse(95% 100% at 50% 0%);
+	}
 
-.hover-trigger .hover-target {
-	display: none;
-}
+	.hover-trigger .hover-target {
+		display: none;
+	}
 
-.hover-trigger:hover .hover-target {
-	display: block;
-}
+	.hover-trigger:hover .hover-target {
+		display: block;
+	}
 </style>
