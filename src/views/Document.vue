cb45068d2cc3280280b95f4e9e48260c3602e57f
Denisa Mensatorisová <32281312+DeMensa123@users.noreply.github.com>
2021-03-15 10:56:21 +0100

    FRON-34 Implement in-text matches

--- src/views/Document.vue
+++ src/views/Document.vue
@@ -82,10 +82,10 @@ export default {
 		compiledHighlight() {
 			return {
 				render: (h) => {
-					return h(Vue.compile(`<p>${this.highlightedText()}</p>`));
+					return h(Vue.compile(`<p class="text-base md:text-lg">${this.highlightedText()}</p>`));
 				},
 			};
-		},
+		},		
 	},
 	mounted() {
 		// initial spinner
@@ -118,28 +118,155 @@ export default {
 				.get(`/api/documents/${id}`)
 				.then((response) => response.data);
 		},
+		colorForIndex: function(i, asString = true) {
+			const values = this.HSVToRGB((i * 0.618033988749895) % 1.0, 0.5, Math.sqrt(1.0 - ((i * 0.618033988749895) % 0.5)));
+			return asString ? `rgb(${values[0]}, ${values[1]}, ${values[2]})` : values;
+		},
+		clamp: function(val, min, max) {
+			return Math.max(min, Math.min(val, max));
+		},
+		HSVToRGB: function(h, s, v) {
+			if (s == 0) {
+				return [v, v, v];
+			}
+			if (v == 0) {
+				return [0, 0, 0];
+			}
+			const col = [0, 0, 0];
+			const hval = h * 6;
+			const sel = Math.floor(hval);
+			const mod = hval - sel;
+			const v1 = v * (1 - s);
+			const v2 = v * (1 - s * mod);
+			const v3 = v * (1 - s * (1 - mod));
+
+			switch (sel + 1) {
+				case 0:
+					col[0] = v;
+					col[1] = v1;
+					col[2] = v2;
+					break;
+				case 1:
+					col[0] = v;
+					col[1] = v3;
+					col[2] = v1;
+					break;
+				case 2:
+					col[0] = v2;
+					col[1] = v;
+					col[2] = v1;
+					break;
+				case 3:
+					col[0] = v1;
+					col[1] = v;
+					col[2] = v3;
+					break;
+				case 4:
+					col[0] = v1;
+					col[1] = v2;
+					col[2] = v;
+					break;
+				case 5:
+					col[0] = v3;
+					col[1] = v1;
+					col[2] = v;
+					break;
+				case 6:
+					col[0] = v;
+					col[1] = v1;
+					col[2] = v2;
+					break;
+				case 7:
+					col[0] = v;
+					col[1] = v3;
+					col[2] = v1;
+					break;
+			}
+
+			col[0] = Math.floor(this.clamp(col[0], 0, 1) * 255);
+			col[1] = Math.floor(this.clamp(col[1], 0, 1) * 255);
+			col[2] = Math.floor(this.clamp(col[2], 0, 1) * 255);
+			return col;
+		},
+		mergeIntervals: function (intervals) {
+			
+			if(intervals.length <= 1)
+				return intervals;
+
+			let stack = [];
+			let top = null;
+
+			intervals = intervals.sort(function (a, b) {
+				if (a.from === b.from){
+					return +(a.to > b.to) || +(a.to === b.to) - 1;
+				}
+				return +(a.from > b.from) || - 1;
+			});
+			
+			intervals[0].matches = [{doc: intervals[0].name, per: intervals[0].percentage}]
+			stack.push(intervals[0]);
+			
+			for (var i = 1; i < intervals.length; i++) {
+				top = stack[stack.length - 1];
+				
+				if (top.to < intervals[i].from) {
+					intervals[i].matches = [{doc: intervals[i].name, per: intervals[i].percentage}]
+					stack.push(intervals[i]);
+				}
+				
+				else if (top.to <= intervals[i].to){
+					top.to = intervals[i].to;
+					top.matches.push({doc: intervals[i].name, per: intervals[i].percentage})
+					stack.pop();
+					stack.push(top);
+				}
+				else if(top.to > intervals[i].to){
+					top.matches.push({doc: intervals[i].name, per: intervals[i].percentage})
+					stack.pop();
+					stack.push(top);
+				}
+			}
+			
+			return stack;
+		},
+		toggleTooltip: function(docs){
+
+			const docs_unique = docs.filter((elem, index) => docs.findIndex(obj => obj.doc === elem.doc) === index)
+			let text = ''
+
+			for (var i = 0; i < docs_unique.length; i++){
+				text = text.concat('Zhoda so súborom: ', docs_unique[i].doc, ' (',  docs_unique[i].per, '%)<br>')
+			}
+			
+			return(text)
+		},
 		highlightedText: function () {
-			const indices = this.document.matched_docs
+			
+			const indices = this.document.result.matched_docs
 				.map((matched_doc) =>
-					matched_doc.matches.map((match) => ({
+					matched_doc.intervals.map((match) => ({
 						name: matched_doc.name,
-						from: match.char_from,
-						to: match.char_to,
+						from: match.begin,
+						to: match.end,
+						percentage: this.$filters.roundToTwoDecimals(this.$filters.toNumber(matched_doc.percentage)),
+						color: this.colorForIndex(this.document.result.matched_docs.map(d => d.name).indexOf(matched_doc.name))
 					}))
 				)
 				.flat();
+		
+			const intervals = this.mergeIntervals(indices)
 
-			indices.sort((a, b) => b.to - b.from - (a.to - a.from));
-
-			const subStringsToReplace = indices.map((h) => ({
+			const subStringsToReplace = intervals.map((h) => ({
 				text: this.document.text.substring(h.from, h.to + 1),
-				doc: h.name,
-			}));
+				docs: h.matches,
+				color: h.color,				
+			}));	
+			
 			return subStringsToReplace.reduce(
 				(string, substring) =>
 					string.replace(
 						new RegExp(substring.text, 'g'),
-						`<span v-tooltip.top="'Zhoda so súborom: ${substring.doc}'" class="text-red-500 font-bold">${substring.text}</span>`
+						`<span v-tooltip.top="'${this.toggleTooltip(substring.docs)}'" class="font-bold" style="color:${substring.color};">${substring.text}</span>`
 					),
 				escape(this.document.text)
 			);
@@ -147,3 +274,32 @@ export default {
 	},
 };
 </script>
+
+<style>
+.tooltip {	 
+	background: black;
+	color: black;
+	border-radius: 10px;
+	padding: 5px 5px ;
+}
+
+.tooltip .tooltip-arrow {
+  width: 0;
+  height: 0;
+  border-style: solid;
+  position: absolute;
+  margin: 5px;
+  border-color: black;
+}
+
+.tooltip[x-placement^="top"] .tooltip-arrow {
+  border-width: 5px 5px 0 5px;
+  border-left-color: transparent !important;
+  border-right-color: transparent !important;
+  border-bottom-color: transparent !important;
+  bottom: -5px;
+  left: calc(50% - 5px);
+  margin-top: 0;
+  margin-bottom: 0;
+}
+</style>
