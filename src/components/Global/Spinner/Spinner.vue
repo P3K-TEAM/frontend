8983cd1399f85641614bbe3ed0c46da0c7656865
František Gič <mcfreddie777@gmail.com>
2021-03-16 16:31:45 +0100

    FRON-35 Refactor Spinner component to support reusability

--- src/components/Global/Spinner/Spinner.vue
+++ src/components/Global/Spinner/Spinner.vue
@@ -1,31 +1,32 @@
 <template>
 	<div>
 		<svg
-			v-if="isLoading"
+			v-if="active"
 			viewBox="0 0 100 100"
 			xmlns="http://www.w3.org/2000/svg"
 			class="text-primary-500 stroke-current spinner w-16"
 		>
-			<circle
-				cx="50"
-				cy="50"
-				r="45"
-			/>
+			<circle cx="50" cy="50" r="45" />
 		</svg>
-		<div v-if="isMessageActive">
+		<div v-if="message">
 			<p class="m-4 text-xl text-white text-center">
-				{{ infoMessage }}
+				{{ message }}
 			</p>
 		</div>
 	</div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-
 export default {
-	computed: {
-		...mapGetters(['isMessageActive', 'isLoading', 'infoMessage']),
+	props: {
+		active: {
+			type: Boolean,
+			required: true,
+		},
+		message: {
+			type: String,
+			default: '',
+		},
 	},
 };
 </script>
