f36310645f4cfa43d0df429f393a32361d69d032
František Gič <mcfreddie777@gmail.com>
2021-03-30 02:53:24 +0200

    FRON-38 Create tabs component

---
+++ src/components/Global/Tabs/Tabs.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="flex flex-row">
+		<TabItem
+			v-for="(tab, index) in tabs"
+			:key="index"
+			:tab="tab"
+			:active="selectedTab === tab.key"
+			:is-first="index === 0"
+			:is-last="index === tabs.length - 1"
+			@click.native="markTabAsSelected(tab)"
+		/>
+	</div>
+</template>
+
+<script>
+import TabItem from '@/components/Global/Tabs/TabItem';
+
+export default {
+	components: {
+		TabItem
+	},
+	props: {
+		tabs: {
+			type: Array,
+			required: true
+		},
+		selectedTab: {
+			type: String,
+			required: true
+		}
+	},
+	methods: {
+		markTabAsSelected(tab) {
+			if (!tab.disabled) {
+				this.$emit('update:selectedTab', tab.key);
+			}
+		}
+	}
+};
+</script>
